1ba9a3028586275807a868bae2690d02124b81b0
Emily Lau <lauyingwen@gmail.com>
2019-02-23 13:35:23 +0800

    Updated Query 1, 2, 3 & 4

--- Queries.sql
+++ Queries.sql
@@ -1,17 +1,5 @@
-/*Query 1 - Top Country with Rock Music Listeners? */
-SELECT DISTINCT c.Country, COUNT(c.CustomerId) As NoOfMusicListener
-FROM Customer c
-JOIN Invoice i ON c.CustomerId = i.CustomerId
-JOIN InvoiceLine il ON i.InvoiceId = il.InvoiceId
-JOIN Track t ON il.TrackId = t.TrackId
-JOIN Genre g ON t.GenreId = g.GenreId
-WHERE t.GenreId = 1
-GROUP BY c.Country
-ORDER BY NoOfMusicListener DESC
-
-
-/* Query 1 - Top 5 Customers */
-SELECT c.ContactName, c.Country, SUM(od.Quantity) As QuantityPurchased
+/* Query 1 - Top 5 customers with the most purchases */
+SELECT c.CompanyName, c.Country, SUM(od.Quantity) As QuantityPurchased
 FROM Customers c
 JOIN Orders o ON c.CustomerId = o.CustomerId
 JOIN OrderDetails od ON o.OrderId = od.OrderId
@@ -19,7 +7,7 @@ GROUP BY c.CustomerId
 ORDER BY  QuantityPurchased DESC
 LIMIT 5
 
-/* Query 2 - Top 5 Countries with the most customers */
+/* Query 2 - Top 5 countries with the most purchases */
 SELECT c.Country, SUM(od.Quantity) As QuantityPurchased
 FROM Customers c
 JOIN Orders o ON c.CustomerId = o.CustomerId
@@ -28,50 +16,24 @@ GROUP BY c.Country
 ORDER BY  QuantityPurchased DESC
 LIMIT 5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*Query 2 - Rock Band with Most Songs? */
-SELECT ar.ArtistId, ar.Name, COUNT(t.TrackId) AS Songs
-FROM Artist ar
-JOIN Album al ON ar.ArtistId = al.ArtistId
-JOIN Track t ON al.AlbumId = t.AlbumId
-WHERE t.GenreId = 1
-GROUP BY ar.ArtistId
-ORDER BY Songs desc
+/* Query 3 - Top 10 best products */
+SELECT ProductName, SUM(Quantity) AS TotalQuantity
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+JOIN OrderDetails od ON o.OrderId = od.OrderId
+JOIN Products p ON od.ProductId = p.ProductId
+JOIN Categories cat ON p.CategoryID = cat.CategoryID
+GROUP BY p.ProductName
+ORDER BY TotalQuantity DESC
 LIMIT 10
 
-/*Query 3 - Artist Who Earned The Most? */
-SELECT ar.Name, SUM(il.UnitPrice * il.Quantity) AS TotalSpending
-FROM Invoice i
-JOIN InvoiceLine il ON il.InvoiceId = i.InvoiceId
-JOIN Track t ON il.TrackId = t.TrackId
-JOIN Album al ON t.AlbumId = al.AlbumId
-JOIN Artist ar ON al.ArtistId = ar.ArtistId
-GROUP BY ar.Name
-Order BY TotalSpending DESC
+/* Query 4 - Top 5 popular category */
+SELECT CategoryName, SUM(Quantity) AS TotalQuantity
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+JOIN OrderDetails od ON o.OrderId = od.OrderId
+JOIN Products p ON od.ProductId = p.ProductId
+JOIN Categories cat ON p.CategoryID = cat.CategoryID
+GROUP BY cat.CategoryName
+ORDER BY TotalQuantity DESC
 LIMIT 5
-
-/*Query 4 - Customer Who Spent the Most with U2 */
-SELECT c.Country, COUNT(c.Country) AS NumberOfBuyer
-FROM Customer c
-JOIN Invoice i ON c.CustomerId = i.CustomerId
-JOIN InvoiceLine il ON i.InvoiceId = il.InvoiceId
-JOIN Track t ON il.TrackId = t.TrackId
-JOIN Album al ON t.AlbumId = al.AlbumId
-JOIN Artist ar ON al.ArtistId = ar.ArtistId
-WHERE ar.Name = 'U2'
-GROUP BY c.Country
-ORDER BY NumberOfBuyer DESC
-LIMIT 10
