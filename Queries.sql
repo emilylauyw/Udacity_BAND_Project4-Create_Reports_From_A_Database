6c8e08bf6c502d0e2f786444cac9d5d5876a489c
Emily Lau <lauyingwen@gmail.com>
2019-02-25 07:58:25 +0800

    Updated Query 1 and Queries formatting

--- Queries.sql
+++ Queries.sql
@@ -1,23 +1,29 @@
 /* Query 1 - Top 5 customers with the most purchases */
 SELECT *
-FROM (SELECT c.CompanyName, c.Country, SUM(od.Quantity) As QuantityPurchased FROM Customers c
-	JOIN Orders o ON c.CustomerId = o.CustomerId
-	JOIN OrderDetails od ON o.OrderId = od.OrderId
-	GROUP BY c.CustomerId) t1
-ORDER BY  QuantityPurchased DESC
-LIMIT 5
+FROM
+  (SELECT c.CompanyName,
+          c.Country,
+          SUM(od.Quantity) AS QuantityPurchased
+   FROM Customers c
+   JOIN Orders o ON c.CustomerId = o.CustomerId
+   JOIN OrderDetails od ON o.OrderId = od.OrderId
+   GROUP BY c.CustomerId) t1
+ORDER BY QuantityPurchased DESC
+LIMIT 5;
 
 /* Query 2 - Top 5 countries with the most purchases */
-SELECT c.Country, SUM(od.Quantity) As QuantityPurchased
+SELECT c.Country,
+       SUM(od.Quantity) AS QuantityPurchased
 FROM Customers c
 JOIN Orders o ON c.CustomerId = o.CustomerId
 JOIN OrderDetails od ON o.OrderId = od.OrderId
 GROUP BY c.Country
-ORDER BY  QuantityPurchased DESC
-LIMIT 5
+ORDER BY QuantityPurchased DESC
+LIMIT 5;
 
 /* Query 3 - Top 10 best products */
-SELECT ProductName, SUM(Quantity) AS TotalQuantity
+SELECT ProductName,
+       SUM(Quantity) AS TotalQuantity
 FROM Customers c
 JOIN Orders o ON c.CustomerID = o.CustomerID
 JOIN OrderDetails od ON o.OrderId = od.OrderId
@@ -25,10 +31,11 @@ JOIN Products p ON od.ProductId = p.ProductId
 JOIN Categories cat ON p.CategoryID = cat.CategoryID
 GROUP BY p.ProductName
 ORDER BY TotalQuantity DESC
-LIMIT 10
+LIMIT 10;
 
 /* Query 4 - Top 5 popular category */
-SELECT CategoryName, SUM(Quantity) AS TotalQuantity
+SELECT CategoryName,
+       SUM(Quantity) AS TotalQuantity
 FROM Customers c
 JOIN Orders o ON c.CustomerID = o.CustomerID
 JOIN OrderDetails od ON o.OrderId = od.OrderId
@@ -36,4 +43,4 @@ JOIN Products p ON od.ProductId = p.ProductId
 JOIN Categories cat ON p.CategoryID = cat.CategoryID
 GROUP BY cat.CategoryName
 ORDER BY TotalQuantity DESC
-LIMIT 5
+LIMIT 5;
